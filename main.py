d96f01786f786c345823fb78a7d91b330a30235b
Thomas <203590@novacollege.nl>
2026-01-16 10:39:20 +0100

    Move welcome messages to pick_difficulty function

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import random
 
 def pick_random_number(bereik):
-    print("Welkom bij Warmer-Kouder.")
-    print("Raad welk nummer het is terwijl wij je vertellen of het warmer of kouder is.")
     random.randint(1, bereik)
 
 def get_value(text, data_type):
@@ -13,12 +11,14 @@ def get_value(text, data_type):
             print("Dit is een incorrect datatype, probeer het opnieuw!")
 
 def pick_difficulty():
+    print("Welkom bij Warmer-Kouder.")
+    print("Raad welk nummer het is terwijl wij je vertellen of het warmer of kouder is.")
     print("Op welke difficulty wil je spelen?")
     print("1) makkelijk (10 pogingen, nummer tussen 1 en 100)")
     print("2) normaal (6 pogingen, nummer tussen 1 en 100)")
     print("3) moeilijk (15 pogingen, nummer tussen 1 en 1000)")
 
-    choice = get_value(int, "")
+    choice = get_value("", int)
 
     if choice == 1:
         pogingen = 10
@@ -31,3 +31,8 @@ def pick_difficulty():
         bereik= 1000
 
     return pogingen, bereik
+
+pogingen, bereik = pick_difficulty()
+pick_random_number(bereik)
+
+print(pogingen, bereik)
