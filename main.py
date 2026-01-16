725aa5c56a99fdd7e13b9c4a84fc2397369c1baa
Thomas <203590@novacollege.nl>
2026-01-16 09:18:13 +0100

    [UPDATE] pick_random_number to use a range
    Updated pick_random_number function to accept a range parameter.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-def pick_random_number():
+def pick_random_number(bereik):
     print("Welkom bij Warmer-Kouder.")
     print("Raad welk nummer het is terwijl wij je vertellen of het warmer of kouder is.")
-    random.randint(1, 1000)
+    random.randint(1, bereik)
